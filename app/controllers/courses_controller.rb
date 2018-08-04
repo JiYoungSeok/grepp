20b42e974a3196e3eceb72ebebfd3cc097089aa1
JiYoungSeok <jiys5164@naver.com>
2018-08-04 10:08:08 +0900

    2018/08/04 Modify SQL query order by

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -15,10 +15,12 @@ class CoursesController < ApplicationController
 	end
 
 	def show
-		sql = "SELECT (A.student_count - B.student_count) AS student_change, A.student_count, A.update_at \
-		   	   FROM Sales A, Sales B \
-		   	   WHERE A.course_id = #{params[:course_id]} AND A.course_id = B.course_id AND A.update_at = B.update_at + interval '1 days' AND A.update_at > now() + interval '-7 days' \
-		   	   GROUP BY A.update_at, A.course_id, A.student_count, B.student_count"
+		sql = "SELECT (A.student_count - B.student_count) AS student_change, A.student_count, A.update_at
+		   	   FROM Sales A, Sales B
+		   	   WHERE A.course_id = #{params[:course_id]} AND A.course_id = B.course_id AND A.update_at = B.update_at + interval '1 days' AND A.update_at > now() + interval '-7 days'
+		   	   GROUP BY A.update_at, A.course_id, A.student_count, B.student_count
+		   	   ORDER BY A.update_at
+		   	   LIMIT 7"
 
 		@course_student_change = Sale.find_by_sql(sql)		   	 
 	end
