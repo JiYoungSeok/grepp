3dd6bdf63efe8ca9368dba08bd3efea050b016de
JiYoungseok <jiys5164@naver.com>
2018-08-21 10:37:05 +0900

    2018/08/21 Each course's accumulated revenue

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,40 +1,43 @@
 class CoursesController < ApplicationController
-	include Crawling
+   include Crawling
 
-	def index
-        each_course_revenue = Sale.group(:course_id).sum(:daily_revenue)
+   def index
+      @each_course_accumulated_revenue = []
+      @each_course_revenue = Sale.group(:course_id).sum(:daily_revenue).sort_by {|key, value| value}.reverse
+      @each_course_revenue.each do |key, value|
+         course = Course.find(key)
+         sale = Sale.find_by(course_id: key,
+                             update_at: Time.now.strftime("%Y-%m-%d"))
+         unless sale.nil?
+            each_course = {id: key,
+                           url: course.url,
+                           title: course.title,
+                           price: sale.price,
+                           student_count: sale.student_count,
+                           accumulated_revenue: value}
+         end
+         @each_course_accumulated_revenue << each_course
+      end
 
-        each_course_revenue.each do |e|
-        	course = Course.find(e.course_id)
+      puts @each_course_accumulated_revenue
+   end
 
-        end
-	end
+   def new
+      create
+   end
 
-	def new
-		create
-	end
+   def create
+      Crawling.auto_crawling
+      redirect_to "/"
+   end
 
-	def create
-		Crawling.auto_crawling
-		# sql = "SELECT A.id, ((A.student_count - B.student_count) * A.price) AS revenue
-		# 	   FROM Sales A, Sales B
-		# 	   WHERE A.course_id = B.course_id AND A.update_at = B.update_at + interval '1days'"
-		# sale = Sale.find_by_sql(sql)
-
-		# sale.each do |s|
-		# 	Sale.find(s.id).update(daily_revenue: s.revenue)
-		# end
-		redirect_to "/"
-	end
-
-	def show
-		sql = "SELECT (A.student_count - B.student_count) AS student_change, A.student_count, A.update_at
+   def show
+      sql = "SELECT (A.student_count - B.student_count) AS student_change, A.student_count, A.update_at
 		   	   FROM Sales A, Sales B
 		   	   WHERE A.course_id = #{params[:id]} AND A.course_id = B.course_id AND A.update_at = B.update_at + interval '1 days' AND A.update_at > now() + interval '-7 days'
 		   	   GROUP BY A.update_at, A.course_id, A.student_count, B.student_count
-		   	   ORDER BY A.update_at
-		   	   LIMIT 7"
+		   	   ORDER BY A.update_at"
 
-		@course_student_change = Sale.find_by_sql(sql)		   	 
-	end
+      @course_student_change = Sale.find_by_sql(sql)
+   end
 end
--- app/modules/crawling.rb
+++ app/modules/crawling.rb
@@ -3,125 +3,132 @@ require "nokogiri"
 require 'selenium-webdriver'
 
 module Crawling
-	def self.parse_html(url)
-		begin
-			doc = Nokogiri::HTML(open(url))
+   def self.parse_html(url)
+      begin
+         doc = Nokogiri::HTML(open(url))
 
-			title = doc.css('div#item-header-content h1').text
-			student_count = doc.css('div.students').text.sub('명', '').to_i
-			price_area = doc.css('li.course_price')[0]
-			discount = doc.css('li.course_price').css('del')[0]
+         title = doc.css('div#item-header-content h1').text
+         student_count = doc.css('div.students').text.sub('명', '').to_i
+         price_area = doc.css('li.course_price')[0]
+         discount = doc.css('li.course_price').css('del')[0]
 
-			if discount.nil?
-				price = price_area.css('span.woocommerce-Price-amount')[0]
-				if price.nil?
-					price = 0
-				else
-					price = price.text.sub('₩', '').sub(',', '').to_i
-				end
-			else
-				price = price_area.css('span.woocommerce-Price-amount')[1].text.sub('₩', '').sub(',', '').to_i
-			end
+         if discount.nil?
+            price = price_area.css('span.woocommerce-Price-amount')[0]
+            if price.nil?
+               price = 0
+            else
+               price = price.text.sub('₩', '').sub(',', '').to_i
+            end
+         else
+            price = price_area.css('span.woocommerce-Price-amount')[1].text.sub('₩', '').sub(',', '').to_i
+         end
 
-			course = {'url' => url, 'title' => title, 'price' => price, 'student_count' => student_count}
+         course = {'url' => url, 'title' => title, 'price' => price, 'student_count' => student_count}
 
-			return course
-		rescue
-			parse_html(url)
-		end
-	end
+         return course
+      rescue
+         parse_html(url)
+      end
+   end
 
-	def self.get_current_page_number(driver)
-		html = driver.page_source
-		doc = Nokogiri::HTML(html)
+   def self.get_current_page_number(driver)
+      html = driver.page_source
+      doc = Nokogiri::HTML(html)
 
-		page_number_area = doc.css('div.pagination-links span')
-		current_page_number = page_number_area[0].text.to_i + page_number_area[1].text.to_i
-	
-		return current_page_number
-	end
+      page_number_area = doc.css('div.pagination-links span')
+      current_page_number = page_number_area[0].text.to_i + page_number_area[1].text.to_i
 
-	def self.get_last_page_number(driver)
-		html = driver.page_source
-		doc = Nokogiri::HTML(html)
+      return current_page_number
+   end
 
-		last_page_number = doc.css('div.pag-count')[0].text.to_i
-	
-		return last_page_number
-	end
+   def self.get_last_page_number(driver)
+      html = driver.page_source
+      doc = Nokogiri::HTML(html)
 
-	def self.get_each_page_courses_url(driver)
-		html = driver.page_source
-		doc = Nokogiri::HTML(html)
-		urls = []
+      last_page_number = doc.css('div.pag-count')[0].text.to_i
 
-		urls_list = doc.css('div.item-title a')
+      return last_page_number
+   end
 
-		urls_list.each do |url|
-			urls.push(url['href'])
-		end
+   def self.get_each_page_courses_url(driver)
+      html = driver.page_source
+      doc = Nokogiri::HTML(html)
+      urls = []
 
-		return urls
-	end
+      urls_list = doc.css('div.item-title a')
 
-	def self.sort_by_alphabetical(driver)
-		sort_xpath = "//li[@id='course-order-select']"
-		sort_btn = driver.find_element(:xpath => sort_xpath)
-		sort_btn.click
-		sleep(3)
-		sort_by_alphabetical_xpath = "//option[@value='alphabetical']"
-		sort_by_alphabetical_btn = driver.find_element(:xpath => sort_by_alphabetical_xpath)
-		sort_by_alphabetical_btn.click
-		sleep(3)
-	end
+      urls_list.each do |url|
+         urls.push(url['href'])
+      end
 
-	def self.get_all_urls_by_selenium
-		options = Selenium::WebDriver::Chrome::Options.new
-		options.add_argument('--headless')
-		driver = Selenium::WebDriver.for :chrome, options: options
-		all_urls = []
+      return urls
+   end
 
-		driver.get 'https://www.inflearn.com/all-courses/'
-		sort_by_alphabetical(driver)
+   def self.sort_by_alphabetical(driver)
+      sort_xpath = "//li[@id='course-order-select']"
+      sort_btn = driver.find_element(:xpath => sort_xpath)
+      sort_btn.click
+      sleep(3)
+      sort_by_alphabetical_xpath = "//option[@value='alphabetical']"
+      sort_by_alphabetical_btn = driver.find_element(:xpath => sort_by_alphabetical_xpath)
+      sort_by_alphabetical_btn.click
+      sleep(3)
+   end
 
-		next_page_xpath = "//div[@id='course-dir-list']/div[@id='pag-top']/div[@class='pagination-links']/a[@class='next page-numbers']"
-		last_page_number = get_last_page_number(driver)
+   def self.get_all_urls_by_selenium
+      options = Selenium::WebDriver::Chrome::Options.new
+      options.add_argument('--headless')
+      driver = Selenium::WebDriver.for :chrome, options: options
+      all_urls = []
 
-		while true
-			sleep(3)
-			current_page_number = get_current_page_number(driver)
-			all_urls = all_urls + get_each_page_courses_url(driver)
+      driver.get 'https://www.inflearn.com/all-courses/'
+      sort_by_alphabetical(driver)
 
-			if current_page_number == last_page_number
-				break
-			end
+      next_page_xpath = "//div[@id='course-dir-list']/div[@id='pag-top']/div[@class='pagination-links']/a[@class='next page-numbers']"
+      last_page_number = get_last_page_number(driver)
 
-			next_page_btn = driver.find_element(:xpath => next_page_xpath)
-			next_page_btn.click
-		end
+      while true
+         sleep(3)
+         current_page_number = get_current_page_number(driver)
+         all_urls = all_urls + get_each_page_courses_url(driver)
 
-		driver.quit
-		return all_urls
-	end
+         if current_page_number == last_page_number
+            break
+         end
 
-	def self.auto_crawling
-		all_urls = get_all_urls_by_selenium
+         next_page_btn = driver.find_element(:xpath => next_page_xpath)
+         next_page_btn.click
+      end
 
-		all_urls.each do|url|
-			course = parse_html(url)
-			new_course = Course.new(title: course['title'], url: course['url'])
-			new_course.save
+      driver.quit
+      return all_urls
+   end
 
-			course_id = Course.find_by(url: course['url'].downcase).id
-			prev_student_count = Sale.find_by(course_id: course_id, update_at: Date.yesterday)
-			if prev_student_count
-				daily_revenue = (course['student_count'].to_i - prev_student_count.student_count.to_i) * course['price'].to_i
-				new_sale = Sale.new(price: course['price'], student_count: course['student_count'], update_at: Time.now.strftime("%Y-%m-%d"), daily_revenue: daily_revenue ,course_id: course_id)
-			else
-				new_sale = Sale.new(price: course['price'], student_count: course['student_count'], update_at: Time.now.strftime("%Y-%m-%d"),course_id: course_id)
-			end
-			
-			new_sale.save
-		end
-	end
+   def self.auto_crawling
+      all_urls = get_all_urls_by_selenium
+
+      all_urls.each do |url|
+         course = parse_html(url)
+         new_course = Course.new(title: course['title'], url: course['url'])
+         new_course.save
+
+         course_id = Course.find_by(url: course['url'].downcase).id
+         prev_student_count = Sale.find_by(course_id: course_id, update_at: Date.yesterday)
+         if prev_student_count
+            daily_revenue = (course['student_count'].to_i - prev_student_count.student_count.to_i) * course['price'].to_i
+            new_sale = Sale.new(price: course['price'],
+                                student_count: course['student_count'],
+                                update_at: Time.now.strftime("%Y-%m-%d"),
+                                daily_revenue: daily_revenue,
+                                course_id: course_id)
+         else
+            new_sale = Sale.new(price: course['price'],
+                                student_count: course['student_count'],
+                                update_at: Time.now.strftime("%Y-%m-%d"),
+                                course_id: course_id)
+         end
+
+         new_sale.save
+      end
+   end
 end
